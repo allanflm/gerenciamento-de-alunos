44e222fa34ef4bbfed3ebde6138c8bf79a7e1e76
Allan <allanfelipedk@gmail.com>
2024-10-26 16:02:28 -0300

    feat: instalando o gorm e conectando a api com o banco de dados

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/allanflm/api-go-gim/database"
 	"github.com/allanflm/api-go-gim/models"
 	"github.com/allanflm/api-go-gim/routes"
 )
 
 func main() {
+	database.ConectaComBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "Allan", CPF: "12345678921", RG: "00000000"},
 		{Nome: "Felipe", CPF: "000 000 000 00 ", RG: "00000021"},
