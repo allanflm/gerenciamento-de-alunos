c58a4106e8e5f2d0a8428596bcaf7641ad3e48b4
Allan <allanfelipedk@gmail.com>
2024-10-27 20:32:28 -0300

    feat: deletando, editando e buscando aluno por cpf

--- routes/routes.go
+++ routes/routes.go
@@ -8,8 +8,17 @@ import (
 func HangleRequests() {
 	r := gin.Default()
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
+
 	r.GET("/:nome", controllers.Saudacao)
+
 	r.POST("/alunos", controllers.CriaNovoAluno)
+
 	r.GET("/alunos/:id", controllers.BuscaAlunoPorId)
+
+	r.DELETE("/alunos/:id", controllers.DeletaAluno)
+
+	r.PATCH("/alunos/:id", controllers.EditarAluno)
+
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
 	r.Run()
 }
